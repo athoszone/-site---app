f86ddc36ff7764c753f7f0816df50b305c9dd3f6
athoszone <marcotheon@gmail.com>
2026-05-19 18:38:50 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Athos Forecast API is running');
 });
